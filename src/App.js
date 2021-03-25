79a842c2c7f777430f6da97f7a477c5d60946713
Edward116 <bondarchukeduard@gmail.com>
2021-03-25 22:16:26 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import Display from "./Components/Display/DisplayOfStopwatch";
+import Buttons from "./Components/Buttons/ButtonsOfStopwatch";
+import {Observable} from "rxjs";
 
 function App() {
+    const [time, setTime] = useState({h:0, m:0, s:0});
+    const [state, setState] = useState(0);
+
+
+    const run = () => {
+        if(time.m === 60){
+            time.h++;
+            time.m = 0;
+        }
+        if(time.s === 60){
+            time.m++;
+            time.s = 0;
+        }
+        time.s++;
+        return setTime({s:time.s, m:time.m, h:time.h});
+    };
+
+    const start = () => {
+        run();
+        setState(1);
+        setInterval(run, 1000)
+    }
+
+    const stop = () => {
+        clearInterval(run)
+        setState(0);
+        setTime({h:0, m:0, s:0})
+    }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Display time={time}/>
+     <Buttons stop={stop} start={start} state={state}/>
     </div>
   );
 }
---
+++ src/Components/Buttons/ButtonsOfStopwatch.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+
+
+
+const Buttons = (props) => {
+    return (
+        <div>
+            {(props.state === 0) ?
+                <div>
+                    <button onClick={props.start}>Start</button>
+                    <button disabled={true}>Stop</button>
+                </div>
+                : ""
+            }
+
+            {(props.state === 1) ?
+                <div>
+                    <button onClick={props.reset}>Reset</button>
+                    <button onClick={props.stop}>Stop</button>
+                </div> : ""
+            }
+
+        </div>
+    );
+}
+
+
+export default Buttons;
